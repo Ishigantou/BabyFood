06563c92ebc002c0e6e9fd74ad2e558bd5666234
Seiei Higa <hanachin@gmail.com>
2012-11-17 19:06:05 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BabyFood::Application.config.secret_key_base = 'e687fd6149045d9e255f40bfa60980a26ed56b5efc17ed245deda6401437d5210c718a6fa65e4fb7e9e32ca96c101ed1752dd78a51f04f8ab2e393b74e5b1cd2'
