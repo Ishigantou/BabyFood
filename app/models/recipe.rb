2f99649be922b7193c148fc42fa458792dabb706
Seiei Higa <hanachin@gmail.com>
2012-11-22 21:16:02 +0900

    implement Recipe.recoomended when stage is 離乳食 初期（5〜6ヶ月）

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 # coding: utf-8
 class Recipe < ActiveRecord::Base
   has_and_belongs_to_many :categories
@@ -15,16 +17,31 @@ class Recipe < ActiveRecord::Base
 
   def self.recommended stage = nil
     staple = main = side = nil
-    self.where(stage: stage).shuffle.each do |r|
-      categories = r.categories.map(&:title)
-      if categories.include?('主食')
-        staple = r unless staple
-      elsif categories.include?('主菜')
-        main = r unless main
-      elsif categories.include?('副菜')
-        side = r unless side
+
+    if stage == '離乳食 初期（5〜6ヶ月）'
+      self.where(stage: stage).shuffle.each do |r|
+        categories = r.categories.map(&:title)
+        if categories.include?('初めての離乳食')
+          staple = r unless staple
+        elsif categories.include?('野菜')
+          main = r unless main
+        elsif categories.include?('タンパク質')
+          side = r unless side
+        end
+        return [staple, main, side] if staple && main && side
       end
-      return [staple, main, side] if staple && main && side
+    else
+      self.where(stage: stage).shuffle.each do |r|
+        categories = r.categories.map(&:title)
+        if categories.include?('主食')
+          staple = r unless staple
+        elsif categories.include?('主菜')
+          main = r unless main
+        elsif categories.include?('副菜')
+          side = r unless side
+        end
+        return [staple, main, side] if staple && main && side
+      end
     end
     []
   end
