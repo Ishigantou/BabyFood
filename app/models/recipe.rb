694e625a76d7198504d11607598aa2d00b508b08
Seiei Higa <hanachin@gmail.com>
2012-11-22 09:27:03 +0900

    add Recipe.category

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,14 @@
 class Recipe < ActiveRecord::Base
   has_and_belongs_to_many :categories
+
+  scope :category, -> cat {
+    if cat.is_a? ActiveRecord::Relation or cat.is_a? Array
+      cat.inject(all) {|cats, c|  cats.category(c)}
+    else
+      where([
+        'id in (select cr.recipe_id from `categories_recipes` as cr where cr.category_id = ?)',
+        cat.id
+      ])
+    end
+  }
 end
